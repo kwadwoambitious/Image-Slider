965ae9690be1a8f32559ef7da26f7b7fd881e76e
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-05-30 23:34:43 +0000

    Changed the buttons style

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const card = document.querySelector('.card');
 //the cards to be displayed have been stored in an array
 let cards = [{
   name: 'First Card',
-  'background-color': 'Brown',
-  color: 'White'
+  'background-color': '#00ffff',
+  color: 'Black'
 },{
   name: 'Second Card',
   'background-color': 'Black',
@@ -16,11 +16,11 @@ let cards = [{
 }, {
   name: 'Third Card',
   'background-color': 'Green',
-  color: 'Black'
+  color: 'White'
 }, {
   name: 'Fourth Card',
   'background-color': '#008080',
-  color: 'Black'
+  color: 'White'
 }, {
   name: 'Fifth Card',
   'background-color': 'Blue',
@@ -28,6 +28,10 @@ let cards = [{
 }, {
   name: 'Sixth Card',
   'background-color': 'darkorange',
+  color: 'Black'
+}, {
+  name: 'Seventh Card',
+  'background-color': 'aquamarine',
   color: 'Black'
 }];
 
@@ -53,11 +57,11 @@ function updateFunction(){
 
 //display the next card when the next-button is clicked
 nextButton.addEventListener('click', () => {
-  previousButton.style.visibility = 'visible';
+  previousButton.disabled = false;
     i ++;
-    if(i === 5){
+    if(i === cards.length-1){
       updateFunction();
-      nextButton.style.visibility = 'collapse';
+      nextButton.disabled = true;
     }
     else{
       updateFunction();
@@ -66,16 +70,16 @@ nextButton.addEventListener('click', () => {
 
 //set the visibility of previous-button to collapse when the page first loads
 if(i === 0){
-  previousButton.style.visibility = 'collapse';
+  previousButton.disabled = true;
 }
 
 //display the previous card when the previous-button is clicked
 previousButton.addEventListener('click', () => {
-    nextButton.style.visibility = 'visible';
+    nextButton.disabled = false;
     i --;
     if(i === 0){
       updateFunction();
-      previousButton.style.visibility = 'collapse';
+      previousButton.disabled = true;
     }
     else if(i >= 0){
       updateFunction();
